4197816adecf7b0dcf707ff9904e76fcc05d6e46
Kumar Shubam <ks92217@gmail.com>
2019-08-22 02:32:38 +0530

    Design Current Weather UI

---
+++ app/src/main/java/com/shubham/forecast/internal/Glide/ForcastAppGlideModule.kt
@@ -0,0 +1,7 @@
+package com.shubham.forecast.internal.Glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class ForcastAppGlideModule:AppGlideModule()
--- app/src/main/java/com/shubham/forecast/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/shubham/forecast/ui/weather/current/CurrentWeatherFragment.kt
@@ -1,12 +1,15 @@
 package com.shubham.forecast.ui.weather.current
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.shubham.forecast.R
+import com.shubham.forecast.internal.Glide.GlideApp
 import com.shubham.forecast.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.current_weather_fragment.*
 import kotlinx.coroutines.launch
@@ -44,7 +47,62 @@ class CurrentWeatherFragment : ScopedFragment(),KodeinAware {
 
             if(it == null) return@Observer
 
-            textView.text = it.toString()
-        }) }
+            group_loading.visibility = View.GONE
+            updateLocation("Los Angeles")
+            updateDateToToday()
+            updateTemperature(it.temperature,it.feelsLikeTemperature)
+            updateCondition(it.conditionText)
+            updateWind(it.windDirection,it.windSpeed)
+            updatePrecipitation(it.precipitationVolume)
+            updateVisibility(it.visibilityDistance)
+
+            GlideApp.with(this@CurrentWeatherFragment)
+                .load("http:${it.conditionIconUrl}")
+                .into(imageView_condition_icon)
+
+        })
     }
+
+    private fun chooseLocalizedUnitAbbreviation(metric: String, imperial: String):String{
+        return if (viewModel.isMetric) metric else imperial
+    }
+
+    private fun updateLocation(location:String){
+
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+        }
+
+    private fun updateDateToToday(){
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Today"
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateTemperature(temperature: Double, feelsLikeTemperature: Double){
+
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("°C","°F")
+        textView_temperature.text = "$temperature$unitAbbreviation"
+        textView_feels_like_temperature.text = "feels like $feelsLikeTemperature$unitAbbreviation"
+    }
+
+    private fun updateCondition(condition: String){
+        textView_condition.text = condition
+    }
+
+    private fun updatePrecipitation(precipitationVolume: Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("mm","in")
+        textView_precipitation.text = "Precipitation: $precipitationVolume$unitAbbreviation"
+    }
+
+    private fun updateWind(windDirection:String, windSpeed:Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("kph","mph")
+        textView_wind.text = "Wind: $windDirection, $windSpeed$unitAbbreviation"
+    }
+
+    private fun updateVisibility(visibilityDistance:Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("km","mi.")
+        textView_visibility.text = "Visibility: $visibilityDistance$unitAbbreviation"
+    }
+
+}
 
